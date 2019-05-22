708688b523d9524dc838fc607c63132b9017c836
YaroslavAnt <antonchyk.yar@gmail.com>
2019-05-22 15:51:40 +0300

    Updates

---
+++ precache-manifest.79c1afea9c23d2cfb7cb9f4cbe781960.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70eb58385c306bd0569e0cbe22f094c3",
+    "url": "/booking_test/index.html"
+  },
+  {
+    "revision": "9bd8940beb3014281a1d",
+    "url": "/booking_test/static/css/main.c746be8b.chunk.css"
+  },
+  {
+    "revision": "64f1242e8f65e1580c73",
+    "url": "/booking_test/static/js/2.56125fc1.chunk.js"
+  },
+  {
+    "revision": "9bd8940beb3014281a1d",
+    "url": "/booking_test/static/js/main.466409be.chunk.js"
+  },
+  {
+    "revision": "5ecbfbf617d6b5d77cc3",
+    "url": "/booking_test/static/js/runtime~main.ce44fa32.js"
+  }
+]);
