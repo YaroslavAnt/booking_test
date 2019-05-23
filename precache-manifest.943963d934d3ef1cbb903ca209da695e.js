df9d62f94730befad9b3c761c8211eaf1ac5db55
YaroslavAnt <antonchyk.yar@gmail.com>
2019-05-23 17:37:55 +0300

    Updates

---
+++ precache-manifest.943963d934d3ef1cbb903ca209da695e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "154c25409e96b5be1b4e980f23ba9841",
+    "url": "/booking_test/index.html"
+  },
+  {
+    "revision": "8fe081ebd1bb375b33ab",
+    "url": "/booking_test/static/css/main.15eade0b.chunk.css"
+  },
+  {
+    "revision": "5c3d7dc0b92caa248719",
+    "url": "/booking_test/static/js/2.b7dc29b5.chunk.js"
+  },
+  {
+    "revision": "8fe081ebd1bb375b33ab",
+    "url": "/booking_test/static/js/main.d49a43be.chunk.js"
+  },
+  {
+    "revision": "5ecbfbf617d6b5d77cc3",
+    "url": "/booking_test/static/js/runtime~main.ce44fa32.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/booking_test/precache-manifest.1534de65fc1482488ae9db19feff0afc.js"
+  "/booking_test/precache-manifest.943963d934d3ef1cbb903ca209da695e.js"
 );
 
 self.addEventListener('message', (event) => {
