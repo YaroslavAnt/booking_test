a36346f5692acef8db1c565a35ed46de3f47651a
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2019-05-28 09:13:20 +0300

    Updates

--- precache-manifest.348ff33b79fe2234fd9d9a15f4e828a2.js
+++ precache-manifest.348ff33b79fe2234fd9d9a15f4e828a2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7059ffc0588a38d096ab594c23a5851f",
+    "revision": "0b5164253872016a375c8ca62d883f3c",
     "url": "/booking_test/index.html"
   },
   {
-    "revision": "8ee37674c236efbab558",
+    "revision": "4398d35187395eeb0382",
     "url": "/booking_test/static/css/main.15eade0b.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/booking_test/static/js/2.a5ceb27b.chunk.js"
   },
   {
-    "revision": "8ee37674c236efbab558",
-    "url": "/booking_test/static/js/main.1e7f26bb.chunk.js"
+    "revision": "4398d35187395eeb0382",
+    "url": "/booking_test/static/js/main.3858c158.chunk.js"
   },
   {
     "revision": "5ecbfbf617d6b5d77cc3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/booking_test/precache-manifest.c96fca928c6df4073232ef48bd3a3902.js"
+  "/booking_test/precache-manifest.348ff33b79fe2234fd9d9a15f4e828a2.js"
 );
 
 self.addEventListener('message', (event) => {
