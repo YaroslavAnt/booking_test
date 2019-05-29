33c115b423cfcce3bac57edccdcff1c9cf8bb909
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2019-05-29 18:05:04 +0300

    Updates

--- precache-manifest.7108b8d6ab82b285b30c92d7a5a7e7d7.js
+++ precache-manifest.7108b8d6ab82b285b30c92d7a5a7e7d7.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2fa23decfa7ca19bec42870e4e0bc4ae",
+    "revision": "7a4c44aa395c883657ecf9be2eeb222b",
     "url": "/booking_test/index.html"
   },
   {
-    "revision": "494ee00634981a96ffd3",
+    "revision": "fccc1c044e311eda8f26",
     "url": "/booking_test/static/css/main.a91708b6.chunk.css"
   },
   {
-    "revision": "d3232fb7845ccb9a5d83",
-    "url": "/booking_test/static/js/2.b5de4612.chunk.js"
+    "revision": "c330ce13843715f1e676",
+    "url": "/booking_test/static/js/2.3a560645.chunk.js"
   },
   {
-    "revision": "494ee00634981a96ffd3",
-    "url": "/booking_test/static/js/main.2cbcea76.chunk.js"
+    "revision": "fccc1c044e311eda8f26",
+    "url": "/booking_test/static/js/main.5f8ef68e.chunk.js"
   },
   {
     "revision": "5ecbfbf617d6b5d77cc3",
